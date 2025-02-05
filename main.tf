edea731de29ecaedd2484587e2e20d54aa35b463
ishaikh-ho <iqbal.shaikh2@digital.homeoffice.gov.uk>
2025-02-05 11:05:08 +0000

    CCL-2019 (#4)
    * CCL-2019: Adding description for Groups and policies
    
    * CCL-2019: Some refactoring
    
    * CCL-2019: fixing typos

--- main.tf
+++ main.tf
@@ -2,8 +2,9 @@ resource "dynatrace_iam_policy" "env_policy" {
   for_each = var.iam_policies
 
   name            = each.key
+  description     = each.value.policy_description
   account         = var.accountUUID
-  statement_query = each.value
+  statement_query = each.value.policy_statement
 }
 
 module "groups_and_bindings" {
@@ -11,6 +12,7 @@ module "groups_and_bindings" {
   for_each = var.groups_and_permissions
 
   group_name                 = each.key
+  group_description          = each.value.group_description
   attached_policies          = each.value.attached_policies
   federated_attribute_values = each.value.federated_attribute_values
   iam_policies               = [for k, v in dynatrace_iam_policy.env_policy : v]
