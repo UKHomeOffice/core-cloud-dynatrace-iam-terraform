03654acc89eceff5ae07857bba88631740416c5f
Babatunde Kassim <babatunde.kassim1@digital.homeoffice.gov.uk>
2025-03-20 08:33:54 +0000

    Add boundary policy resource and input variable
    Signed-off-by: Babatunde Kassim <babatunde.kassim1@digital.homeoffice.gov.uk>

--- main.tf
+++ main.tf
@@ -13,22 +13,30 @@ resource "dynatrace_iam_policy" "env_policy" {
   statement_query = each.value.policy_statement
 }
 
+resource "dynatrace_iam_policy_boundary" "boundaries" {
+  for_each = var.iam_boundary_policies
+
+  name  = each.key
+  query = each.value.policy_query
+
+}
+
 locals {
   groups_helper = flatten([
-    for group_name, group_values in var.groups_and_permissions:
+    for group_name, group_values in var.groups_and_permissions :
+    flatten([
+      for policy_name, policy_values in group_values.attached_policies :
       flatten([
-        for policy_name, policy_values in group_values.attached_policies:
-        flatten([
-          for env_id, env_params in policy_values: {
-            "group_name" = group_name
-            "policy_name" = policy_name
-            "group_description" = group_values.group_description
-            "federated_attribute_values" = group_values.federated_attribute_values
-            "env_id" = env_id
-            "env_params" = env_params
-          }
-        ])
+        for env_id, env_params in policy_values : {
+          "group_name"                 = group_name
+          "policy_name"                = policy_name
+          "group_description"          = group_values.group_description
+          "federated_attribute_values" = group_values.federated_attribute_values
+          "env_id"                     = env_id
+          "env_params"                 = env_params
+        }
       ])
+    ])
   ])
   iam_policies = concat(data.dynatrace_iam_policies.allPolicies.policies, [for k, v in dynatrace_iam_policy.env_policy : v])
 }
@@ -46,7 +54,7 @@ resource "dynatrace_iam_policy_bindings_v2" "cc-policy-bindings" {
     for group_permission in local.groups_helper : "${group_permission.group_name}.${group_permission.policy_name}.${group_permission.env_id}" => group_permission
   })
 
-  group    = element([for item in dynatrace_iam_group.cc-iam-group : item if item["name"] == each.value.group_name], 0).id
+  group = element([for item in dynatrace_iam_group.cc-iam-group : item if item["name"] == each.value.group_name], 0).id
 
   environment = each.value.env_id
 
@@ -54,5 +62,6 @@ resource "dynatrace_iam_policy_bindings_v2" "cc-policy-bindings" {
     id         = element([for item in local.iam_policies : item if item["name"] == each.value.policy_name], 0).id
     parameters = each.value.env_params != null ? each.value.env_params.policy_parameters : null
     metadata   = each.value.env_params != null ? each.value.env_params.policy_metadata : null
+    boundaries = [for item in dynatrace_iam_policy_boundary.boundaries : item.id if item["name"] == each.value.group_name ]
   }
 }
