e96c6b083d31a913748e517e88b82e219c1c19ba
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2025-07-02 12:53:23 +0100

    CCL-3159 modified dynatrace_iam_policy_bindings_v2

--- main.tf
+++ main.tf
@@ -61,10 +61,16 @@ resource "dynatrace_iam_policy_bindings_v2" "cc-policy-bindings" {
   environment = each.value.env_id
 
   policy {
-    id         = element([for item in local.iam_policies : item if item["name"] == each.value.policy_name], 0).id
-    parameters = each.value.env_params != null ? each.value.env_params.policy_parameters : null
-    metadata   = each.value.env_params != null ? each.value.env_params.policy_metadata : null
-    boundaries = [for item in dynatrace_iam_policy_boundary.boundaries : item.id if item.name == each.key]
+    # id         = element([for item in local.iam_policies : item if item["name"] == each.value.policy_name], 0).id
+    id        = lookup({for item in local.iam_policies : item["name"] => item}, each.value.policy_name, null)?.id
+
+    # parameters = each.value.env_params != null ? each.value.env_params.policy_parameters : null
+    parameters = try(each.value.env_params.policy_parameters, null)
+    # metadata   = each.value.env_params != null ? each.value.env_params.policy_metadata : null
+    metadata   = try(each.value.env_params.policy_metadata, null)
+    # boundaries = [for item in dynatrace_iam_policy_boundary.boundaries : item.id if item.name == each.key]
+    boundaries = try([for item in dynatrace_iam_policy_boundary.boundaries : item.id if item.name == each.key], [])
+
   }
 }
 
