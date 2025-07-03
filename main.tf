b761a93d8494c89c0ce57d61018a15070647e4fa
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2025-07-03 11:19:22 +0100

    CCL-3159 modified dynatrace_iam_policy_bindings_v2

--- main.tf
+++ main.tf
@@ -78,8 +78,8 @@ resource "dynatrace_iam_policy_bindings_v2" "cc-policy-bindings" {
   # }
   policy {
     id         = element([for item in local.iam_policies : item if item["name"] == each.value.policy_name], 0).id
-    parameters = each.value.env_params != {} ? each.value.env_params.policy_parameters : {}
-    metadata   = each.value.env_params != {} ? each.value.env_params.policy_metadata : {}
+    parameters = each.value.env_params != null ? each.value.env_params.policy_parameters : {}
+    metadata   = each.value.env_params != null ? each.value.env_params.policy_metadata : {}
     boundaries = [for item in dynatrace_iam_policy_boundary.boundaries : item.id if item.name == each.key]
   }
 
