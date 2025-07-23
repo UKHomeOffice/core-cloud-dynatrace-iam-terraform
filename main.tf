72fb6dc5a70dd26c082acf8d490ed7348ab21a8b
Chris Freeman <christopher.freeman@digital.homeoffice.gov.uk>
2025-07-23 11:37:34 +0100

    test

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "dynatrace_iam_policy_bindings_v2" "cc-policy-bindings" {
   environment = each.value.env_id
 
   policy {
-    id         = element([for item in local.iam_policies : item if item["name"] == each.value.policy_name], 0).id
+    id         = local.iam_policies[each.value.policy_name].id
     parameters = each.value.env_params != null ? each.value.env_params.policy_parameters : null
     metadata   = each.value.env_params != null ? each.value.env_params.policy_metadata : null
     boundaries = [for item in dynatrace_iam_policy_boundary.boundaries : item.id if item.name == each.key]
