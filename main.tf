d836159d29103fbe2289cecf4084ab10c6113c12
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2025-07-28 14:43:42 +0100

    CCL-3159 aded sort for group and policy

--- main.tf
+++ main.tf
@@ -56,12 +56,13 @@ resource "dynatrace_iam_policy_boundary" "boundaries" {
 resource "dynatrace_iam_policy_bindings_v2" "cc-policy-bindings" {
   for_each = local.permission_helper
 
-  group = element([for item in dynatrace_iam_group.cc-iam-group : item if item["name"] == each.value.group_name], 0).id
+  group = element(sort([for item in dynatrace_iam_group.cc-iam-group : item.id
+   if item.name == each.value.group_name]), 0)
 
   environment = each.value.env_id
 
   policy {
-    id         = element([for item in local.iam_policies : item if item["name"] == each.value.policy_name], 0).id
+    id         = element(sort([for item in local.iam_policies : item.id if item.name == each.value.policy_name]), 0)
     parameters = each.value.env_params != null ? each.value.env_params.policy_parameters : null
     metadata   = each.value.env_params != null ? each.value.env_params.policy_metadata : null
     boundaries = [for item in dynatrace_iam_policy_boundary.boundaries : item.id if item.name == each.key]
