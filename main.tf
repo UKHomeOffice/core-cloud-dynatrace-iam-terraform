9a88e9a1f5edfad55d8376f065178ca38bd5b543
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2025-08-01 11:31:25 +0100

    CCL-3159-a investgate on policy bindings

--- main.tf
+++ main.tf
@@ -35,52 +35,64 @@ resource "dynatrace_iam_policy" "env_policy" {
 #   iam_policies = concat(data.dynatrace_iam_policies.allPolicies.policies, [for k, v in dynatrace_iam_policy.env_policy : v])
 # }
 locals {
-  # Permission helper
-  permission_helper = flatten([
+  # ---------------------------------------------
+  # Step 1: Build permission_helper
+  # ---------------------------------------------
+  # This flattens var.groups_and_permissions into a flat map where:
+  #   key   = "<group_name>.<policy_name>.<env_id>"
+  #   value = object containing all relevant attributes for the binding
+  #
+  # Structure of var.groups_and_permissions:
+  # {
+  #   "<group_name>" = {
+  #     group_description          = string
+  #     federated_attribute_values = list(string)
+  #     attached_policies = {
+  #       "<policy_name>" = {
+  #         "<env_id>" = {
+  #           policy_parameters = ...
+  #           policy_metadata   = ...
+  #         }
+  #       }
+  #     }
+  #   }
+  # }
+  permission_helper = merge(flatten([
     for group_name, group_values in var.groups_and_permissions :
     flatten([
       for policy_name, policy_values in group_values.attached_policies :
       {
         for env_id, env_params in policy_values :
-        "${group_name}.${policy_name}.${env_id}" =>
-        {
-          "group_name"                 = group_name
-          "policy_name"                = policy_name
-          "group_description"          = group_values.group_description
-          "federated_attribute_values" = group_values.federated_attribute_values
-          "env_id"                     = env_id
-          "env_params"                 = env_params
+        "${group_name}.${policy_name}.${env_id}" => {
+          group_name                 = group_name
+          policy_name                = policy_name
+          group_description          = group_values.group_description
+          federated_attribute_values = group_values.federated_attribute_values
+          env_id                     = env_id
+          env_params                 = env_params
         }
       }
     ])
-  ])
+  ])...)
 
-  # Grouped permission helper
+  # ---------------------------------------------
+  # Step 2: Group permission_helper by group_name + env_id
+  # ---------------------------------------------
+  # Purpose: Prevent multiple policy bindings from overwriting each other
   grouped_permission_helper = {
     for group_env_key, permission_list in {
-      # Group permissions by group_name + env_id combination
-      for permission in local.permission_helper :
-      "${permission.group_name}-${permission.env_id}" => permission
+      for permission_key, permission_value in local.permission_helper :
+      "${permission_value.group_name}-${permission_value.env_id}" => permission_value...
     } :
     group_env_key => {
       group_name   = permission_list[0].group_name
       env_id       = permission_list[0].env_id
       env_params   = permission_list[0].env_params
-
-      # Collect all policy names for this group/environment
-      policy_names = [for permission_item in permission_list : permission_item.policy_name]
+      policy_names = [for p in permission_list : p.policy_name]
     }
   }
-
-  # Define IAM policies, fetched via a data source
-  iam_policies = flatten([
-    for policy in data.dynatrace_iam_policies.allPolicies.policies :
-    {
-      id   = policy.id
-      name = policy.name
-    }
-  ])
 }
+
 
 resource "dynatrace_iam_group" "cc-iam-group" {
   for_each = var.groups_and_permissions
@@ -100,9 +112,13 @@ resource "dynatrace_iam_policy_boundary" "boundaries" {
 
 }
 
-resource "dynatrace_iam_policy_bindings_v2" "cc-policy-bindings" {
+resource "dynatrace_iam_policy_bindings_v2" "cc_policy_bindings" {
+  # Loop through each grouped permission (group_name + env_id)
   for_each = local.grouped_permission_helper
 
+  # ---------------------------------------------------------
+  # Bind to the correct IAM group based on group_name
+  # ---------------------------------------------------------
   group = element(
     sort([
       for group_item in dynatrace_iam_group.cc-iam-group :
@@ -112,25 +128,34 @@ resource "dynatrace_iam_policy_bindings_v2" "cc-policy-bindings" {
     0
   )
 
+  # ---------------------------------------------------------
+  # Bind to the correct environment
+  # ---------------------------------------------------------
   environment = each.value.env_id
 
-  dynamic "policy" {
-    for_each = [
-      for policy_item in dynatrace_iam_policy.env_policy :
-      policy_item
-      if contains(each.value.policy_names, policy_item.name)
-    ]
+  # ---------------------------------------------------------
+  # Attach all policies for this group/environment combo
+  # ---------------------------------------------------------
+  policy {
+    id = element(
+      sort([
+        for policy_item in dynatrace_iam_policy.env_policy :
+        policy_item.id
+        if policy_item.name in each.value.policy_names
+      ]),
+      0
+    )
 
-    content {
-      id         = policy.value.id
-      parameters = each.value.env_params != null ? each.value.env_params.policy_parameters : null
-      metadata   = each.value.env_params != null ? each.value.env_params.policy_metadata   : null
-      boundaries = sort([
-        for boundary_item in dynatrace_iam_policy_boundary.boundaries :
-        boundary_item.id
-        if boundary_item.name == each.key
-      ])
-    }
+    # Optional parameters and metadata for the policy
+    parameters = lookup(each.value.env_params, "policy_parameters", null)
+    metadata   = lookup(each.value.env_params, "policy_metadata", null)
+
+    # Attach any policy boundaries that match this group/env combo
+    boundaries = sort([
+      for boundary_item in dynatrace_iam_policy_boundary.boundaries :
+      boundary_item.id
+      if boundary_item.name == each.key
+    ])
   }
 }
 
